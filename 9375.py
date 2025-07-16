4948d667a638c8f2a63c30ad7d4f8233ffc14bd6
Your Name <you@example.com>
2025-07-16 18:36:00 +0900

    9375_solution

---
+++ 9375.py
@@ -0,0 +1,31 @@
+# 전체 경우의 수  = (패션 종류당 옷 개수 + 1)들의 곱 - 1
+
+def calculating(fashionCount):
+    result = 1
+
+    for count in fashionCount.values():
+        result *= (count + 1)
+
+    return result - 1
+
+T = int(input())
+
+for i in range(T):
+    #fashion -> count
+    fashionCount = {}
+    fashionVariationCount = 0
+
+    n = int(input())
+
+    for ii in range(n):
+        userInput = input().split()
+
+        key = userInput[1]
+        if not key in fashionCount:
+            fashionCount[key] = 1
+            fashionVariationCount += 1
+        else:
+            fashionCount[key] += 1
+
+
+    print(calculating(fashionCount))
